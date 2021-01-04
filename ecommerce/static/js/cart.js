de3e1f782ab4715bf37b084c19d55ff29a384ae1
sandipan898 <sandipan.das898@gmail.com>
2021-01-05 00:40:34 +0530

    adding javascript

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -20,12 +20,13 @@ for (let i = 0; i < updateBtns.length; i++) {
 function updateUserOrder(productId, action) {
     console.log("User is logged in, sending data...")
 
-    var url = '/update-item/'
+    var url = '/update_item/'
 
     fetch(url, {
         method:'POST',
         headers:{
             'Content-Type':'application/json',
+            'X-CSRFToken': csrftoken
         }, 
         body:JSON.stringify({'productId':productId, 'action':action})
     })
